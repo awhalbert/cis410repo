748cd330bbf6b16d7b9cc3e65ae6e83a4d6ba353
awhalbert <ahalbert@cs.uoregon.edu>
2015-03-03 19:19:01 -0800

    improved color croved color changing algorithm, improved stdout feedback, code reorganization

--- MayaPy/src/mayapy/views/home/specAnlys.py
+++ MayaPy/src/mayapy/views/home/specAnlys.py
@@ -3,68 +3,92 @@ from scipy import arange, ifft
 from numpy import sin, linspace, pi, fft, abs
 from scipy.io.wavfile import read,write
 from nimble import cmds
+import sys
+import colorsys
+# import maya.mel
 
 def main():
-    Fs = 44100;  # sampling rate
+    # set up audio for playback in Maya
+    # audioNode = cmds.sound( offset=10, file=filename)
+    # gPlayBackSlider = maya.mel.eval( '$tmpVar=$gPlayBackSlider' )
+    # cmds.timeControl( gPlayBackSlider, edit=True, sound=audioNode )
 
-    filename = 'They Won\'t Go When I Go.wav'
-    rate,data=read('../../resources/' + filename)
-    cmds.playbackOptions( animationStartTime='1', animationEndTime='8640' )
+
+    # read audio faile into raw data
+    # filename = '../../resources/They Won\'t Go When I Go.wav'
+    filename = '../../resources/Yesterday I Heard the Rain.wav'
+    rate,data=read(filename)
+
+    lastFrame = int(len(data)/float(rate) * 24)
+    cmds.playbackOptions( min='1', max=str(lastFrame))
+
     y=data[:,1]
-    print(data)
-    lungime=len(y)
-    timp=len(y)/44100.
-    t=linspace(0,timp,len(y))
-
-    subplot(2,1,1)
-    plot(t,y)
-    xlabel('Time')
-    ylabel('Amplitude')
-    # subplot(2,1,2)
-    # plotSpectrum(y,Fs)
-    # show()
     bouncingBubble(y)
     exit()
 
 def averageAmplitude(amp, sampleCount):
     avgs = list()
     accum = 0
+    tenPercent = len(amp) // 10
+    curProg = 1
     for i in range(len(amp)):
         accum += abs(amp[i])
         if i % sampleCount == 0 or i == len(amp) - 1:
             avgs.append(accum / sampleCount)
             accum = 0
+        if i % tenPercent == 0 and not (curProg == 0 or curProg==10):
+            sys.stdout.write(str(curProg) + '0%...')
+            curProg += 1
+    print('100%')
     return avgs
 
 def bouncingBubble(amp):
     spf = 1838 # approximate samples per frame
-
     sphere = cmds.polySphere(n='bubble')[0]
-
+    shader = str(cmds.listConnections(cmds.listHistory(sphere,f=1),type='lambert')[0])
+    print('Created ' + sphere + ' with shader ' + shader)
     # cmds.xform('column', t = [0,.5,0], sp=[0,-.5,0]) # move scale pivot to bottom of shape
 
     frameWin = 3
-
-    avgs = averageAmplitude(amp, spf * frameWin) # this averages amplitude every frameWindow frames
-    print(avgs)
+    print('Calculating average amplitude for every ' + str(frameWin)
+          + '-frame ('  + str(frameWin/24.) + ' second) ' + 'window')
+    avgs = averageAmplitude(amp, spf * frameWin)
+    curProg = 1
+    tenPercent = len(avgs) / 10
+    print('Keyframing size translations')
     for i in range(1,len(avgs)):
         h = normalize(avgs, i) * 10.
         cmds.setKeyframe(sphere, v = h, at = 'scaleX', t = i*frameWin)
         cmds.setKeyframe(sphere, v = h, at = 'scaleY', t = i*frameWin)
         cmds.setKeyframe(sphere, v = h, at = 'scaleZ', t = i*frameWin)
+        if i % tenPercent == 0:
+            if not (curProg == 0 or curProg == 10):
+                sys.stdout.write(str(curProg) + '0%...')
+                curProg += 1
+    print('100%')
 
-    frameWin = 24
-    avgs = averageAmplitude(amp, spf*frameWin) # this average amplitude every 24 frames (1 s)
-    print(avgs)
-    shader = str(cmds.listConnections(cmds.listHistory(sphere,f=1),type='lambert')[0])
-    for i in range(2,len(avgs)):
-        hue = normalize(avgs, i)
+    frameWin = 60
+    print('Calculating average amplitude for every ' + str(frameWin)
+          + '-frame ('  + str(frameWin/24.) + ' second) ' + 'window')
+    avgs = averageAmplitude(amp, spf*frameWin)
+    curProg = 1
+    tenPercent = len(avgs) / 10
+    print('Keyframing color translations')
+    for i in range(1,len(avgs)):
+        norm = normalize(avgs, i)
+        rgb = colorsys.hsv_to_rgb(norm, 1, norm) # hue and value both vary with amplitude
         # listConnections returns a list with unicode strings
-        cmds.setAttr(shader + '.color', hue, 0, 0, type='double3')
+        cmds.setAttr(shader + '.color', rgb[0], rgb[1], rgb[2], type='double3')
         cmds.setKeyframe(shader + '.color', at = 'color', t = i*frameWin)
+        if i % tenPercent == 0:
+            if not (curProg == 0 or curProg == 10):
+                sys.stdout.write(str(curProg) + '0%...')
+                curProg += 1
+    print('100%')
 
 def normalize(data, index):
     """
+    Normalizes the value data[index] as a float between 0.0 and 1.0
     :param data: list of data to use for normalization
     :param index: index in data we wish to normalize among the set
     :return: the normalized value, a float between 0.0 and 1.0
@@ -93,4 +117,20 @@ def plotSpectrum(y,Fs):
     plot(frq,abs(Y),'r') # plotting the spectrum
     xlabel('Freq (Hz)')
     ylabel('|Y(freq)|')
+"""
+
+"""
+this code was formerly in main()
+
+# lungime=len(y)
+# timp=len(y)/float(rate)
+# t=linspace(0,timp,len(y))
+
+# subplot(2,1,1)
+# plot(t,y)
+# xlabel('Time')
+# ylabel('Amplitude')
+# subplot(2,1,2)
+# plotSpectrum(y,Fs)
+# show()
 """
